65351c72fbe4c7430eb0b8521975524818dcfcf1
glitterboy16 <villorinaangelandres@gmail.com>
2025-10-10 01:49:09 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Elementos principales
+const barraLateral = document.querySelector('[data-barra-lateral]');
+const enlacesNav = document.querySelectorAll('[enlace-navbar]');
+const paginas = document.querySelectorAll('[data-pagina]');
+
+
+// Navegación entre páginas
+enlacesNav.forEach(enlace => {
+  enlace.addEventListener('click', () => {
+    const paginaId = enlace.getAttribute('enlace-navbar');
+    
+    // Quitar active de todo
+    enlacesNav.forEach(e => e.classList.remove('active'));
+    paginas.forEach(p => p.classList.remove('active'));
+    
+    // Activar enlace y página
+    enlace.classList.add('active');
+    document.querySelector(`[data-pagina="${paginaId}"]`).classList.add('active');
+  });
+});
+
+// Cerrar sidebar al hacer clic fuera (solo móviles)
+document.addEventListener('click', (e) => {
+  if (window.innerWidth <= 1) {
+    const clickDentro = barraLateral.contains(e.target) || botonBarra.contains(e.target);
+    if (!clickDentro) barraLateral.classList.remove('active');
+  }
+});
